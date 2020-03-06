f17254003969e1fe3bacab46d55d79cb35b0d854
Sina <s.j.semnani@gmail.com>
2020-03-06 15:00:52 -0800

    bug fix

--- genienlp/run_lm_finetuning.py
+++ genienlp/run_lm_finetuning.py
@@ -127,10 +127,11 @@ class TextDataset(Dataset):
 
 
 def get_transformer_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, dimension):
+    num_warmup_steps = max(1, num_warmup_steps)
 
     def lr_lambda(current_step):
         current_step += 1
-        return 1. / math.sqrt(dimension) * min(1 / math.sqrt(current_step), current_step / (max(1, num_warmup_steps) * math.sqrt(num_warmup_steps)))
+        return min(1 / math.sqrt(current_step), current_step / (num_warmup_steps * math.sqrt(num_warmup_steps)))
 
     return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
